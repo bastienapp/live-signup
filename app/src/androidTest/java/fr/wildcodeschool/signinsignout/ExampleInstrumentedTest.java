524345452a7a312c362ca72039fbdb0b21a883bf
Bastien Krettly <bastien_krettly@wilder.school>
2018-10-19 14:33:12 +0200

    add photo from galery and camera

--- app/src/androidTest/java/fr/wildcodeschool/signinsignout/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/fr/wildcodeschool/signinsignout/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("fr.wildcodeschool.galaxianscores", appContext.getPackageName());
+        assertEquals("fr.wildcodeschool.signinsignout", appContext.getPackageName());
     }
 }
--- app/src/main/java/fr/wildcodeschool/signinsignout/ProfileActivity.java
+++ app/src/main/java/fr/wildcodeschool/signinsignout/ProfileActivity.java
@@ -1,23 +1,39 @@
 package fr.wildcodeschool.signinsignout;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ProfileActivity extends AppCompatActivity {
 
+    private static final int REQUEST_PICK_IMAGE = 1422;
+    private static final int REQUEST_TAKE_PHOTO = 4152;
+    String mCurrentPhotoPath;
     private FirebaseAuth mAuth;
+    private Uri mPhoto = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,43 +43,48 @@ public class ProfileActivity extends AppCompatActivity {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference userRef = database.getReference("user");
-        final String userId = mAuth.getUid();
-
         Button btGallery = findViewById(R.id.button_gallery);
         btGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, getString(R.string.select_photo)), REQUEST_PICK_IMAGE);
             }
         });
         Button btCamera = findViewById(R.id.button_camera);
         btCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO
+                dispatchTakePictureIntent();
             }
         });
 
         final EditText etName = findViewById(R.id.et_name);
         final EditText etAge = findViewById(R.id.et_age);
 
-        userRef.child(userId).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                UserModel user = dataSnapshot.getValue(UserModel.class);
-                if (user != null) {
-                    etName.setText(user.getName());
-                    etAge.setText(String.valueOf(user.getAge()));
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference userRef = database.getReference("user");
+        final String userId = mAuth.getUid();
+        if (userId != null) {
+            userRef.child(userId).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    UserModel user = dataSnapshot.getValue(UserModel.class);
+                    if (user != null) {
+                        etName.setText(user.getName());
+                        etAge.setText(String.valueOf(user.getAge()));
+                    }
                 }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            }
-        });
+                }
+            });
+        }
 
         Button disconnect = findViewById(R.id.button_disconnect);
         disconnect.setOnClickListener(new View.OnClickListener() {
@@ -85,10 +106,79 @@ public class ProfileActivity extends AppCompatActivity {
                 if (!ageValue.isEmpty()) {
                     age = Integer.parseInt(ageValue);
                 }
-                if (userId != null) {
-                    userRef.child(userId).setValue(new UserModel(name, age));
-                }
+                userRef.child(userId).setValue(new UserModel(name, age));
             }
         });
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "fr.wildcodeschool.signinsignout.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_PICK_IMAGE) {
+                if (data != null && data.getData() != null) {
+                    mPhoto = data.getData();
+                }
+            }
+            if (requestCode == REQUEST_TAKE_PHOTO) {
+                if (mCurrentPhotoPath != null) {
+                    File file = new File(mCurrentPhotoPath);
+                    mPhoto = Uri.fromFile(file);
+                }
+            }
+        }
+        if (mPhoto != null) {
+            ImageView photo = findViewById(R.id.iv_photo);
+            Glide.with(ProfileActivity.this).load(mPhoto).into(photo);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            finish();
+        }
     }
 }
